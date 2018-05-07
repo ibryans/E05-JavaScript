1c332ed691c3c7d29826a0cff11e2c77c11695c1
ibryans <bryans0701@gmail.com>
2018-05-07 10:57:57 -0300

    Função xadrez feita

---
+++ xadrez.js
@@ -0,0 +1,11 @@
+var xadrez = function (linhas) {
+  var x = "# # # #";  // Toda linha vai ter esse padrão
+  var space = " ";    // Espaço antes de cada linha de numero ímpar
+
+  for (var i = 0; i < linhas; i++) {
+    if (i % 2 == 0)
+      console.log(x);
+    else
+      console.log(space + x);
+  }
+}
