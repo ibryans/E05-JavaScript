4548638c41f4aa8c6932a02d61dfdef7b17a5db0
ibryans <bryans0701@gmail.com>
2018-05-08 19:34:16 -0300

    Função palindromo feita

---
+++ palindromo.js
@@ -0,0 +1,15 @@
+var palindromo = function (word) {
+
+  // Armazena a palavra ao contrário
+  var wordReverse = "";
+
+  for (let i = word.length-1; i >= 0; i--) {
+    wordReverse += word[i];
+  }
+
+  if (wordReverse == word)
+    return true;
+  else
+    return false;
+
+}
